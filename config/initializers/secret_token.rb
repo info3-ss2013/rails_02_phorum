ff5104786553f7116d778a83d18bf34ca1512662
B Kleinen <drblinken@gmail.com>
2013-05-31 11:42:59 +0200

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Phorum::Application.config.secret_key_base = '4222ab47385298958281120bf2580ae7bf7f6d0df803498ea8c56ee2b2fba71a09afe3cc5bc23cc127d9446a4841849f5be4148a15cc83e93cdff3f243565037'
